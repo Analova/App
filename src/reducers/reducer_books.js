5cc20ecc273606e1ca6cce0cf9af94f74b640c0f
Ana Nikolova <ani.nik19@gmail.com>
2019-09-26 10:37:26 +0300

    displayed the bookList

---
+++ src/reducers/reducer_books.js
@@ -0,0 +1,8 @@
+export default function() {
+  return [
+    { title: "JavaScript the good parts" },
+    { title: "Harry Poter" },
+    { title: "The dark Tower" },
+    { title: "Step by step" }
+  ];
+}
