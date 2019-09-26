f10d7cc2bd2f44d83f48faa4c4219c5405e14723
Ana Nikolova <ani.nik19@gmail.com>
2019-09-26 16:44:45 +0300

    display a separate book

--- src/reducers/reducer_books.js
+++ src/reducers/reducer_books.js
@@ -1,8 +1,8 @@
 export default function() {
   return [
-    { title: "JavaScript the good parts" },
-    { title: "Harry Poter" },
-    { title: "The dark Tower" },
-    { title: "Step by step" }
+    { title: "JavaScript the good parts", pages: 101 },
+    { title: "Harry Poter", pages: 476 },
+    { title: "The dark Tower", pages: 350 },
+    { title: "Step by step", pages: 987 }
   ];
 }
